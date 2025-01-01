2db9b0333e384eac614137d0acd5abcdf3266610
vansh <vanshraghav2018@gmail.com>
2025-01-01 23:33:11 +0530

    Resume Updated

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -65,7 +65,7 @@ const Contact = () => {
           <textarea name="message" placeholder='Message' cols="30" rows="10" required></textarea>
 
           <ReCAPTCHA
-            sitekey="6Ld5iqsqAAAAAMM731CE24GHQl4Iq0R87MS0udL3"
+            sitekey="6LcBiasqAAAAACaQKhKjpPgh2Zm9X3B2C68-V6sv"
             onChange={handleCaptchaChange}
           />
 
